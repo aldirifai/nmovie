a812259021bff6e7f25247a9edcaa6ccf624ffb1
Muhamad Aldi Rifai <37617582+aldirifai@users.noreply.github.com>
2022-07-09 12:09:15 +0700

    add layouts

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,85 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    @include('layouts.styles')
+
+    <title>Midwifery</title>
+</head>
+
+<body class="bg-light">
+    @include('layouts.navbar')
+    <div class="container py-4">
+        @yield('content')
+    </div>
+
+    @include('layouts.scripts')
+
+
+    @yield('js')
+
+    @include('sweetalert::alert')
+    @if ($errors->any())
+        <div id="ERROR_COPY" style="display: none;" class="alert alert-danger">
+            @foreach ($errors->all() as $error)
+                <h6>{{ $error }}</h6>
+            @endforeach
+        </div>
+    @endif
+
+    @if (config('sweetalert.animation.enable'))
+        <link rel="stylesheet" href="{{ config('sweetalert.animatecss') }}">
+    @endif
+    <script src="{{ $cdn ?? asset('vendor/sweetalert/sweetalert.all.js') }}"></script>
+
+    <script type="text/javascript">
+        var cekError = {{ $errors->any() > 0 ? 'true' : 'false' }};
+        var ht = $("#ERROR_COPY").html();
+        if (cekError) {
+            Swal.fire({
+                title: 'Errors',
+                icon: 'error',
+                html: ht,
+                showCloseButton: true,
+            });
+        }
+
+        $('.btn-delete').on('click', function(e) {
+            e.preventDefault();
+            var form = $(this).closest("form");
+            Swal.fire({
+                title: 'Apakah kamu yakin ?',
+                text: "Data akan dihapus dari database",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: "Ya, Hapus !",
+                cancelButtonText: "Batal",
+                closeOnConfirm: false,
+                closeOnCancel: false
+            }).then((result) => {
+                if (result.value) {
+                    console.log(form.attr('action'));
+                    form.submit();
+                } else if (
+                    /* Read more about handling dismissals below */
+                    result.dismiss === Swal.DismissReason.cancel
+                ) {
+                    Swal.fire(
+                        'Dibatalkan !',
+                        'Data tidak berhasil terhapus',
+                        'error'
+                    )
+                }
+            });
+        });
+    </script>
+</body>
+
+</html>
---
+++ resources/views/layouts/styles.blade.php
@@ -0,0 +1,14 @@
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}">
+<link rel="stylesheet" href="{{ asset('vendor/bootstrap-datepicker/css/bootstrap-datepicker.min.css') }}">
+<link rel="stylesheet" href="{{ asset('vendor/datatables/datatables.min.css') }}">
+
+<style>
+    .navbar {
+        background-color: #FAC900 !important;
+    }
+
+    body {
+        font-family: 'Nunito', sans-serif;
+    }
+</style>
